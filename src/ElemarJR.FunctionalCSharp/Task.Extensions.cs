cc81620768c28841b41feddf8b1f3934b36e6392
Elemar Rodrigues Severo Jr <elemarjr@gmail.com>
2017-03-06 19:20:43 -0300

    Task as container

---
+++ src/ElemarJR.FunctionalCSharp/Task.Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ElemarJR.FunctionalCSharp
+{
+    public static class TaskExtensions
+    {
+        public static async Task<TResult> Map<TInput, TResult>(
+            this Task<TInput> task,
+            Func<TInput, TResult> func
+        ) => func(await task);
+
+        public static Task<TResult> Map<TInput, TResult>(
+            this Task<TInput> task,
+            Func<Exception, TResult> failure,
+            Func<TInput, TResult> success
+        ) => task.ContinueWith(t => t.Status == TaskStatus.Faulted
+            ? failure(t.Exception)
+            : success(t.Result)
+            );
+        public static async Task<TResult> Bind<TInput, TResult>(
+            this Task<TInput> task,
+            Func<TInput, Task<TResult>> func
+        ) => await func(await task);
+
+        public static Task<T> OrElse<T>(
+            this Task<T> task,
+            Func<Task<T>> fallback
+        ) => task.ContinueWith(t =>
+            t.Status == TaskStatus.Faulted
+            ? fallback()
+            : Task.FromResult(t.Result)
+        ).Unwrap();
+
+    }
+}
---
+++ src/ElemarJR.FunctionalCSharp/Task.Linq.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using ElemarJR.FunctionalCSharp;
+
+namespace System.Linq
+{
+    public static partial class LinqExtensions
+    {
+        public static async Task<TResult> Select<TInput, TResult>(
+            this Task<TInput> task,
+            Func<TInput, TResult> func
+        ) => await task.Map(func);
+
+        public static async Task<NewT> SelectMany<T, R, NewT>(
+            this Task<T> task,
+            Func<T, Task<R>> bind,
+            Func<T, R, NewT> projector
+        ) 
+        {
+            var t = await task;
+            var r = await bind(t);
+            return projector(t,  r);
+        }
+    }
+}
